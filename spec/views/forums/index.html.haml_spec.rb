3f061c361857d24ce4eaf54775df9b0de3113bb8
Gustavo Souza <gustavo.do.r.souza@gmail.com>
2015-09-29 01:32:34 -0300

    Fix a date problem in a spec

--- spec/views/forums/index.html.haml_spec.rb
+++ spec/views/forums/index.html.haml_spec.rb
@@ -1,13 +1,13 @@
 ## DEPRECATION NOTICE: Do not add new tests to this file!
 ##
-## View and controller tests are deprecated in the Growstuff project. 
-## We no longer write new view and controller tests, but instead write 
-## feature tests (in spec/features) using Capybara (https://github.com/jnicklas/capybara). 
-## These test the full stack, behaving as a browser, and require less complicated setup 
-## to run. Please feel free to delete old view/controller tests as they are reimplemented 
-## in feature tests. 
+## View and controller tests are deprecated in the Growstuff project.
+## We no longer write new view and controller tests, but instead write
+## feature tests (in spec/features) using Capybara (https://github.com/jnicklas/capybara).
+## These test the full stack, behaving as a browser, and require less complicated setup
+## to run. Please feel free to delete old view/controller tests as they are reimplemented
+## in feature tests.
 ##
-## If you submit a pull request containing new view or controller tests, it will not be 
+## If you submit a pull request containing new view or controller tests, it will not be
 ## merged.
 
 
@@ -45,7 +45,7 @@ describe "forums/index" do
     it "displays posts" do
       assert_select "table"
       rendered.should have_content @post.subject
-      rendered.should have_content Date.today.to_s(:short)
+      rendered.should have_content Time.zone.today.to_s(:short)
     end
 
     it "displays comment count" do
